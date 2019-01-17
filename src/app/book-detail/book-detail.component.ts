e7fb128a83592fe6b8443308ca84fe2d36ec5225
mtking78 <mtking78@gmail.com>
2019-01-17 13:33:28 -0700

    Ang6 Component and Material to display book details.

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailComponent implements OnInit {
 
-  constructor() { }
+  // Variable to hold book data GET from API
+  book = {};
 
+  constructor(private route:ActivatedRoute, private api: ApiService) { }
+
+  // Function to get book details from the API
+  getBookDetails(id) {
+    this.api.getBook(id)
+      .subscribe(data => {
+        console.log(data);
+        this.book = data;
+      });
+  }
+
+  // Call the getBookDetails function when the component is initiated
   ngOnInit() {
+    this.getBookDetails(this.route.snapshot.params['id']);
   }
 
 }
